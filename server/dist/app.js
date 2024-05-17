ebc6d207bea04b7797ca921c5cb69361d87b0dd2
aniket panchal <aniketpanchal07@gmail.com>
2024-05-17 12:11:38 +0530

    done with imports

--- server/dist/app.js
+++ server/dist/app.js
@@ -1,24 +1,78 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-// import cors from "cors";
-// import morgan from "morgan";
-// import createHttpError, { isHttpError } from "http-errors";
+const cors_1 = __importDefault(require("cors"));
+const morgan_1 = __importDefault(require("morgan"));
+const http_errors_1 = __importStar(require("http-errors"));
 // import session from "express-session";
 // import MongoStore from "connect-mongo";
-// import cookieParser from "cookie-parser"
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config({ path: "../.env" });
 // import noteRoutes from "./dRoutes/noteRoutes";
 // import userRouter from "./dRoutes/userRoutes";
 // import { VerifySession } from "./middleware/verifySessionCookie";
-// import path from 'path';
+const path_1 = __importDefault(require("path"));
 // import  { verifyToken } from "./middleware/verifyJwtCookie";
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, morgan_1.default)("dev"));
+app.use((0, cookie_parser_1.default)());
+app.use((0, cors_1.default)());
+app.enable('trust proxy');
+// const dirname = path.resolve();
+const dirname = path_1.default.dirname(path_1.default.resolve());
+// const parentDirname = path.dirname(dirname);
+// const newPath = path.join(parentDirname, path.basename(dirname));
+// console.log(newPath);
+// use the frontend app
+app.use(express_1.default.static(path_1.default.join(dirname, "/client/dist")));
+console.log(dirname);
+app.get('*', (req, res) => {
+    res.sendFile(path_1.default.join(dirname, '/client/dist/index.html'));
+});
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server");
+});
+// end point middleware
+app.use((res, req, next) => {
+    next((0, http_errors_1.default)(404, "endpoint not found"));
+});
+// error handler middleware
+app.use((error, req, res, next) => {
+    console.error(error);
+    let errorMessage = "an unknown error occurred";
+    let statusCode = 500;
+    if ((0, http_errors_1.isHttpError)(error)) {
+        statusCode = error.status;
+        errorMessage = error.message;
+    }
+    res.status(statusCode).json({ error: errorMessage });
 });
 exports.default = app;
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,12 @@
 
 import express, { Express, NextFunction, Request, Response } from "express";
-// import cors from "cors";
-// import morgan from "morgan";
+import cors from "cors";
+import morgan from "morgan";
 
-// import createHttpError, { isHttpError } from "http-errors";
+import createHttpError, { isHttpError } from "http-errors";
 // import session from "express-session";
 // import MongoStore from "connect-mongo";
-// import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser"
 
 import dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
@@ -16,16 +16,52 @@ dotenv.config({ path: "../.env" });
 // import userRouter from "./dRoutes/userRoutes";
 // import { VerifySession } from "./middleware/verifySessionCookie";
 
-// import path from 'path';
+import path from 'path';
 // import  { verifyToken } from "./middleware/verifyJwtCookie";
 
 const app: Express = express();
 
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(cors());
+app.enable('trust proxy')
+
+// const dirname = path.resolve();
+
+const dirname = path.dirname(path.resolve());
+// const parentDirname = path.dirname(dirname);
+// const newPath = path.join(parentDirname, path.basename(dirname));
+// console.log(newPath);
+
+// use the frontend app
+app.use(express.static(path.join(dirname, "/client/dist")));
+console.log(dirname)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(dirname, '/client/dist/index.html'));
+});
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
 });
 
+// end point middleware
+app.use((res, req, next) => {
+    next(createHttpError(404, "endpoint not found"))
+});
+
+// error handler middleware
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    let errorMessage = "an unknown error occurred";
+    let statusCode = 500;
+
+    if (isHttpError(error)) {
+        statusCode = error.status;
+        errorMessage = error.message;
+    }
+    res.status(statusCode).json({ error: errorMessage });
+});
 
 
 export default app;
