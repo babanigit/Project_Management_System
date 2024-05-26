9b6d26d60e4cf28dfc0c3586f9a402c07281b7bc
aniket panchal <aniketpanchal07@gmail.com>
2024-05-26 17:00:19 +0530

    done with from

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
+import { AddClientModel } from "./components/AddClientModel";
 import { Clients } from "./components/Clients";
 import { Header } from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -25,20 +25,17 @@ const cache = new InMemoryCache({
 const client = new ApolloClient({
   uri: "http://localhost:3000/graphql",
   // cache: new InMemoryCache(),
-  cache
+  cache,
 });
 
 const App = () => {
-
-  
   return (
     <>
       <ApolloProvider client={client}>
         <Header />
         <div className="container">
-
+          <AddClientModel />
           <Clients />
-
         </div>
       </ApolloProvider>
     </>
