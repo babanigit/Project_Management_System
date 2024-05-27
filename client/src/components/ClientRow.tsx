7a2f2ac6ddc51eb3218c3f85afe8695eccbd9d4f
babanigit <Aniketgithub07>
2024-05-27 10:51:18 +0530

    from windows

--- client/src/components/ClientRow.tsx
+++ client/src/components/ClientRow.tsx
@@ -1,11 +1,44 @@
-import { FaEnvelope, FaIdBadge, FaPhone, FaTrash } from "react-icons/fa";
+import {
+  //  FaEnvelope, FaIdBadge, FaPhone,
+
+  FaTrash,
+} from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+
+import { DELETE_CLIENT } from "../mutations/ClientMutations";
+
 import { IClient } from "./Clients";
+import { GET_CLIENT } from "../queries/ClientQuery";
 
 interface IProps {
   client: IClient;
 }
 
 const ClientRow = ({ client }: IProps) => {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    // refetchQueries:[{query:GET_CLIENT}]
+
+    update(cache, { data: { deleteClient } }) {
+      // Use type assertion to define the shape of data returned by cache.readQuery
+      const cachedData = cache.readQuery<{ clients: IClient[] }>({ query: GET_CLIENT });
+
+      if (cachedData) {
+        cache.writeQuery({
+          query: GET_CLIENT,
+          data: {
+            clients: cachedData.clients.filter((c) => c.id !== deleteClient.id),
+          },
+        });
+      }
+    },
+  });
+
+  const handleDeleteClient = () => {
+    // Invoke the deleteClient mutation function
+    deleteClient();
+  };
+
   return (
     <>
       {/* <h5 className="mt-5">Client Information</h5>
@@ -26,11 +59,13 @@ const ClientRow = ({ client }: IProps) => {
         <td> {client.email} </td>
         <td> {client.phone} </td>
         <td>
-            <button className="btn btn-danger btn-sm">
-                <FaTrash />
-            </button>
+          <button
+            onClick={handleDeleteClient}
+            className="btn btn-danger btn-sm"
+          >
+            <FaTrash />
+          </button>
         </td>
-
       </tr>
     </>
   );
