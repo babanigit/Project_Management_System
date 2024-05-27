3ef16c4324d81e5b2503f4d88e0d2c2bfdd25947
aniket panchal <aniketpanchal07@gmail.com>
2024-05-27 15:18:21 +0530

    added form windows

---
+++ client/src/components/ClientInfo.tsx
@@ -0,0 +1,6 @@
+
+export const ClientInfo = () => {
+  return (
+    <div>ClientInfo</div>
+  )
+}
---
+++ client/src/components/DeleteProjectButton.tsx
@@ -0,0 +1,7 @@
+
+
+export const DeleteProjectButton = () => {
+  return (
+    <div>DeleteProjectButton</div>
+  )
+}
