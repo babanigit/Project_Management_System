8df0f149acf9fe835eea9c2417dd6f3f551fca17
babanigit <Aniketgithub07>
2024-05-27 12:17:42 +0530

    done with add form

--- client/src/components/AddClientModel.tsx
+++ client/src/components/AddClientModel.tsx
@@ -1,20 +1,75 @@
+import { useMutation } from "@apollo/client";
 import { ChangeEvent, useState } from "react";
 import { FaUser } from "react-icons/fa";
+import { ADD_CLIENT } from "../mutations/clientMutations";
+import { GET_CLIENT } from "../queries/ClientQuery";
 
+// Define interface for formData
+interface FormData {
+  name?: string;
+  email?: string;
+  phone?: string;
+}
 
 export const AddClientModel = () => {
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState<FormData>({});
+
+  const [addClient] = useMutation(ADD_CLIENT, {
+    variables: {
+      name: formData.name,
+      email: formData.email,
+      phone: formData.phone,
+    },
+    update(cache, { data: { addClient } }) {
+      const { clients } = cache.readQuery<{ clients: unknown[] }>({
+        query: GET_CLIENT,
+      }) || { clients: [] };
+
+      cache.writeQuery({
+        query: GET_CLIENT,
+        data: {
+          clients: [...clients, addClient],
+        },
+      });
+    },
+  });
 
   const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
     console.log(formData);
+
+    console.log(formData.name, formData.email, formData.phone);
   };
 
-  const handleClicked = async (e: { preventDefault: () => void; }) => {
-    e.preventDefault()
+  const handleClicked = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
 
-    console.log(formData)
-  }
+    if (
+      formData.name === " " ||
+      formData.email === " " ||
+      formData.phone === " "
+    ) {
+      return alert("please fill in all fields");
+    }
+
+    // addClient(formData.name, formData.email, formData.phone);
+    addClient({
+      variables: {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+      },
+    });
+
+    console.log("clicked");
+    console.log(formData);
+
+    setFormData({
+      name: " ",
+      email: " ",
+      phone: " ",
+    });
+  };
 
   return (
     <>
@@ -50,15 +105,14 @@ export const AddClientModel = () => {
               ></button>
             </div>
             <div className="modal-body">
-              <form
-              onSubmit={handleClicked}
-              action="">
+              <form onSubmit={handleClicked} action="">
                 <div className="mb 3">
                   <label className="form-label"> Name</label>
                   <input
                     type="text"
                     className="form-control"
                     id="name"
+                    value={formData.name}
                     onChange={handleChange}
                   />
                 </div>
@@ -69,6 +123,7 @@ export const AddClientModel = () => {
                     type="email"
                     className="form-control"
                     id="email"
+                    value={formData.email}
                     onChange={handleChange}
                   />
                 </div>
@@ -79,14 +134,19 @@ export const AddClientModel = () => {
                     type="phone"
                     className="form-control"
                     id="phone"
+                    value={formData.phone}
                     onChange={handleChange}
                   />
                 </div>
 
-                  <button
+                <button
                   type="submit"
+                  onClick={handleClicked}
                   data-bs-dismiss="modal"
-                  className=" btn btn-secondary">Submit</button>
+                  className=" btn btn-secondary"
+                >
+                  Submit
+                </button>
               </form>
             </div>
           </div>
