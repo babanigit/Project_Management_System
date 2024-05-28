83513c042538ad4cb7a97421a53c6d292c172c66
aniket panchal <aniketpanchal07@gmail.com>
2024-05-28 17:25:35 +0530

    added form linux

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,7 @@
 import logo from "../assets/logo.png";
 
 export const Header = () => {
-  return (
+  return ( 
     <div className="navbar bg-light mb-4 p-0 ">
       <div className="container">
         <a className="navbar-brand" href="/">
